c0bf8d70e7699786494128ab6c6ec9e1687c8b9f
Kasiet <kasiet.peaksoft@gmail.com>
2022-09-16 22:50:03 +0600

    Text Added_test

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello world!_Tilek");
+        System.out.println("Hello world!_Test");
     }
 }
